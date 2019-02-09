03bc23c5976445090a6e60b30e2cb76e4b5c09e5
Gaston Snaider <gnsnaider@gmail.com>
2019-02-09 12:37:02 -0300

    First commit

---
+++ ch1_the_basics/5/write.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main() {
+    bool graduated = true;
+    char initial = 'G';
+    int age = 25;
+    double weight = 61.05;
+    std::string name = "Gaston";
+
+    std::cout << "Name: " << name << "\nInitial: " << initial << "\nAge: " << age << "\nWeight: " << weight << "\nGraduated: " << graduated << "\n";
+}
+
