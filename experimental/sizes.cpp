8055902098fc5054fcb37489d2358a8122261012
Gaston Snaider <gnsnaider@gmail.com>
2019-02-10 19:30:21 -0300

    sizes experiment

---
+++ experimental/sizes.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "sizeof(bool): " << sizeof(bool) << '\n';
+
+	cout << "sizeof(short int): " << sizeof(short int) << '\n';
+
+	// cout << "sizeof(unsigned short int): " << sizeof(unsigned short int) << '\n';
+
+	cout << "sizeof(int): " << sizeof(int) << '\n';
+
+	// cout << "sizeof(unsigned int): " << sizeof(unsigned int) << '\n';
+
+	cout << "sizeof(long int): " << sizeof(long int) << '\n';
+
+	// cout << "sizeof(unsigned long int): " << sizeof(unsigned long int) << '\n';
+
+	cout << "sizeof(long long int): " << sizeof(long long int) << '\n';
+
+	// cout << "sizeof(unsigned long long int): " << sizeof(unsigned long long int) << '\n';
+
+	cout << "sizeof(char): " << sizeof(char) << '\n';
+
+	cout << "sizeof(float): " << sizeof(float) << '\n';
+
+	cout << "sizeof(double): " << sizeof(double) << '\n';
+
+	cout << "sizeof(long double): " << sizeof(long double) << '\n';
+
+	
+}
