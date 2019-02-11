c0f02399374c599c65be7f6469172bde4a9817ed
Gaston Snaider <gnsnaider@gmail.com>
2019-02-10 21:33:19 -0300

    Exercise 4

---
+++ x9_structures_unions_and_enumerations/4/date.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+
+using namespace std;
+
+enum Month { JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER };
+
+struct Date {
+	int year;
+	Month month;
+	int day;
+
+	Date(int y, Month m, int d) : year {y}, month {m}, day {d} {}
+};
+
+Date* read_date() {
+	cout << "Year: ";
+	int year;
+	cin >> year;
+
+	cout << "Month: ";
+	int month;
+	cin >> month;
+		
+	cout << "Day: ";
+	int day;
+	cin >> day;
+
+
+	cout << year << "-" << month << "-" << day << "\n";
+
+	// Date d(year, static_cast<Month>(month), day);
+	return new Date(year, static_cast<Month>(month), day);
+}
+
+void print_date(const Date* date) {
+	cout << date->year << "-" << date->month << "-" << date->day << "\n";
+}
+
+int main () {
+	// Date date = read_date();
+	Date* date = read_date();
+	print_date(date);
+}
